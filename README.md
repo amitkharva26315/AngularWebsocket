a614e503588606d9058d3ddd8a5d9d51e550a159
Amit Kharva <amitkharva@outlook.com>
2020-09-12 16:29:31 +0530

    First Commit

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+Ref links:
+
+https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
+
+https://tutorialedge.net/typescript/angular/angular-websockets-tutorial/#:~:text=WebSockets%20in%20Angular&text=This%20is%20a%20library%20for,when%20'X'%20event%20occurs.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WebsocketService } from "./websocket.service";
+import { ChatService } from "./chat.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [WebsocketService, ChatService]
 })
 export class AppComponent {
-  title = 'websocket';
+  constructor(private chatService: ChatService) {
+    chatService.messages.subscribe(msg => {
+      console.log("Response from websocket: " + msg);
+    });
+  }
+
+  private message = {
+    author: "tutorialedge",
+    message: "this is a test message"
+  };
+
+  sendMsg() {
+    let msg = document.getElementById('message');
+    console.log(msg["value"]);
+    console.log("new message from client to websocket: ", this.message);
+    this.chatService.messages.next(msg["value"]);
+    this.message.message = "";
+  }
 }
